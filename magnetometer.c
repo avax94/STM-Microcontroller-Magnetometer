37bcd902f60281a3d3c0e5c4052708cd970a2031
avax94 <stefanovic.milan94@gmail.com>
2017-03-13 14:19:37 +0100

    magnetometer refactored

--- magnetometer.c
+++ magnetometer.c
@@ -29,7 +29,6 @@ char xyz[6];
 char output[10];
 int reading_xyz = 0;
 int stop_generated = 0;
-//void poll_value();
 
 void debug(char* d) {
  clear_lcd();
@@ -84,9 +83,7 @@ void event_handler() iv  IVT_INT_I2C2_EV ics ICS_AUTO {
              i2c_send_addr(address, r_notw); //if we already sent register address we now do read or right
          }
 
-
-         START_TR = 0;
-
+         START_TR = 0; //clear start flag
          break;
     case ADDRESS_SENT:
          dummy1 = I2C2_SR1; //need to read sr1 followed by read sr2 so we clean flags
@@ -99,46 +96,43 @@ void event_handler() iv  IVT_INT_I2C2_EV ics ICS_AUTO {
              if(curr_transfer != transfer_count) { //transmiter
                  i2c_send(transfer[curr_transfer]);
                  curr_transfer = curr_transfer +  1;
-              } else if(BTF_I2C == 1) {
+              } else if(BTF_I2C == 1) { //STOP when BTF is set
                     i2c_stop_();
               }
          } else {
             if(curr_transfer == 0) {
                i2c_send(reg_addr);
                curr_transfer = curr_transfer + 1;
-            } else if(BTF_I2C == 1) {
+            } else if(BTF_I2C == 1) { //take action when BTF
                reg_send = 1;
                curr_transfer = 0;
-               if(r_notw == 1)
-                 i2c_start_();
+               
+               if(r_notw == 1) //repetead start if RD is desired action
+                   i2c_start_();
             }
          }
          break;
     case RECEIVED:
          if(curr_transfer != transfer_count) {
-
-          
-           transfer[curr_transfer] = i2c_recv();
+             transfer[curr_transfer] = i2c_recv();
              curr_transfer = curr_transfer + 1;
 
-           if(curr_transfer == transfer_count) {// || transfer_count == curr_transfer) && transfer_count != 1) {
-                ACKN = 0;
-                i2c_stop_();
-                state_ = 6;
-            }//IntToHex(transfer[curr_transfer], output);
-            //debug(output);
-           state_ = 5;
+             if(curr_transfer == transfer_count) {
+                 ACKN = 0; //send NACK
+                 i2c_stop_(); //stop communication
+                 state_ = 5;
+             }
 
              if(curr_transfer == transfer_count) {
-              if(reading_xyz == 1) {
-                reading_xyz = 0;
-                calcAngle(transfer);
-              }
-            }
+                 if(reading_xyz == 1) { //STATE: reading magnetometer data
+                     reading_xyz = 0;
+                     calcAngle(transfer);
+                 }
+             }
          }  else  {
             i2c_recv();
          }
-        break;
+         break;
     case STOPING:
          if(I2C2_SR1bits.STOPF == 1)
              I2C2_CR1bits.STOP_ = 0;
@@ -150,17 +144,19 @@ void event_handler() iv  IVT_INT_I2C2_EV ics ICS_AUTO {
     EnableInterrupts();
 }
 
+//send MAG3110 reg addr
 void send_reg_addr_async(char reg_a) {
      reg_send = 0;
      reg_addr = reg_a;
 }
 
+//used for debugging
 void read_who_am_i() {
     char result = 0;
-   char st[5];
-     cnt = 0;
+    char st[5];
+    cnt = 0;
     EnableInterrupts();
-    debug("Uso sam ovde...");
+    debug("Entered...");
     Delay_ms(1000);
     i2c_start_async();
     send_reg_addr_async(WHO_AM_I_ADDR);
@@ -186,6 +182,7 @@ void write_reg(char reg, char* d, int cnt) {
     i2c_send_async(d, cnt);
 }
 
+//sync reading magnet data
 int read_xyz(char *d) {
      int dreg = 1;
      int bitt = 0;
@@ -204,14 +201,13 @@ int read_xyz(char *d) {
 void configure_exti() {
      char xyz[6];
      RCC_APB2ENRbits.SYSCFGEN = 1; //enable syscfg periph
-     RCC_AHB1ENRbits.GPIOEEN = 1; //enable PA clock
-     GPIOE_MODERbits.MODER10 = 0; //set PA8 as input
+     RCC_AHB1ENRbits.GPIOEEN = 1; //enable PE clock
+     GPIOE_MODERbits.MODER10 = 0; //set PE10 as input
      GPIOE_PUPDRbits.PUPDR10 = 0x1; //pullup
      GPIOE_OTYPERbits.OT10 = 0x0;
      GPIOE_OSPEEDRbits.OSPEEDR10 = 0x2;
-     //GPIO_Digital_Input(&GPIOE_BASE, _GPIO_PINMASK_10);
 
-     SYSCFG_EXTICR3bits.EXTI10 = 0x4; //set PA8 to be EXTI pin
+     SYSCFG_EXTICR3bits.EXTI10 = 0x4; //set PE10 to be EXTI pin
      EXTI_FTSR = 0x00000000;
      EXTI_RTSR = 0x00000400; //detect rising edge
      EXTI_IMR |= 0x00000400; //unmask bit
@@ -219,13 +215,11 @@ void configure_exti() {
      EnableInterrupts();
 
      read_xyz(xyz); //we need to read xyz to clear INT1 bit because if its set we won't be able to detect rising edge
- }
-
-
+}
 
 void init_magnetometer() {
-     char creg1 = 0b10001001;
-     char creg2 = 0b10100000;
+     char creg1 = 0b10001001; //ACTIVE, 2.5Hz, 32 Sample Ratio
+     char creg2 = 0b10100000; //AutoReset, RAW
      char c1;
      char c2;
      char o[5];
@@ -236,64 +230,24 @@ void init_magnetometer() {
      Delay_us(500);
      is_configured = 1;
      configure_exti();
+}
 
- }
- void interrupt_handle() iv IVT_INT_EXTI15_10 ics ICS_AUTO {
+void interrupt_handle() iv IVT_INT_EXTI15_10 ics ICS_AUTO {
    if(is_configured == 0) {
      return;
    }
    
-   EXTI_PR.B10 = 1;
-   reading_xyz = 1;
-   read_reg(OUT_X_MSB_ADDR, xyz, 6);
- }
+   EXTI_PR.B10 = 1; // clear interrupt flag
+   reading_xyz = 1; // enter STATE: reading magnetometer data
+   read_reg(OUT_X_MSB_ADDR, xyz, 6); //read data
+}
 
- void printToLCD(char *d) {
+void printToLCD(char *d) {
    write_lcd(d[2]);
    write_lcd(d[3]);
- }
-void dealwithit() {
-     char xyz[6];
-     char output[5];
-     float heading;
-     float declAngle = 0.069;
-     int x;
-     int y;
-     float headingD;
-     int angle;
-
-     xyz[0] = xyz[1] = xyz[2] = xyz[3] = xyz[4] = xyz[5] = 0;
-
-     Delay_ms(2);
-
-x = (((int)xyz[0]) << 8) | xyz[1];
-         y = (((int)xyz[2]) << 8) | xyz[3];
-         //y = x - (int)((0x0000029A + 0x00000438) >> 1);
-         y -= (int)((0x0000029A + 0x00000438) >> 1);
-         x -= (int)((0x0000FD00 + 0x0000FB4F) >> 1);
-        //    clear_lcd();
-         //set_position(0, 0);
-          //IntToHex(x, output);
-          //write_string(output);
-          //IntToHex(y, output);
-          //write_lcd(' ');
-          //write_string(output);
-         heading = atan2(y, x);
-         heading = heading - declAngle;
-         //FloatToStr(heading, output);
-         //write_string(output);
-         if(heading < 0)   heading += 2*3.14;
-         if(heading < 0)   heading += 2*3.14;
-         // Check for wrap due to addition of declination.
-         if(heading > 2*3.14)heading -= 2*3.14;
-         headingD = heading * 180 / 3.14;
-         angle = (int)headingD;
-
-         IntToStr(angle, output);
-         clear_lcd();
-         set_position(0, 0);
-         write_string(Ltrim(output));
 }
+
+//sync polling magnetometer data
 void poll_value() {
      int dreg;
      char xyz[6];
@@ -303,18 +257,15 @@ void poll_value() {
        dreg = 1;
        read_reg(DR_STATUS_ADDR, &dreg, 1);
        Delay_ms(2);
-       IntToHex(dreg, output);
-       //debug(output);
        bitt = dreg >> 3 & 1;
        if(bitt != 0) {
-          dealwithit();
+          calcAngle(xyz);
           Delay_ms(250);
        }
-
-
      }
 }
 
+//unit test for i2c rd and write
 void test_rd_write() {
      char xyz[6], rd = 3;
      char o[5];
@@ -326,5 +277,4 @@ void test_rd_write() {
      Delay_ms(2000);
      IntToHex(creg1, xyz);
      debug(xyz);
-
 }
